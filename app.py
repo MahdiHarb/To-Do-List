4d1c1fe8fbed28cc72eb6be85d8aec6a01c1da60
Mahdi Harb <mahdiharb0309@gmail.com>
2024-06-27 06:39:57 +0300

    Added the Get all task API

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ CORS(app)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/todo_db"
 mongo = PyMongo(app)
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    result = [{"id": str(task["_id"]), "task": task["task"]} for task in tasks]
+    return jsonify(result), 200
